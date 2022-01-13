9546f90a9b5f222c7ff5021b447a1fb5e4256c71
filipeassaf <filipevalles@gmail.com>
2022-01-13 16:01:11 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,80 @@
+
+#Instalação do Python
+https://www.python.org/
+
+#Instalação do Pycharm
+https://www.jetbrains.com/pt-br/pycharm/download
+
+#-------------------------------------------
+
+print("Hello World")
+
+#-------------------------------------------
+
+#print("Hello World")
+
+print("1")
+print("2")
+print("3")
+print("4")
+
+#-------------------------------------------
+
+#print("Hello World")
+
+'''
+print("1")
+print("2")
+print("3")
+print("4")
+'''
+
+#-------------------------------------------
+
+num1 = 1
+num2 = "dois"
+
+#-------------------------------------------
+
+num1 = 1.123
+num2 = True / False
+
+#-------------------------------------------
+
+num1 = 1
+num2 = 2
+result = num1 + num2
+
+print(result)
+
+#-------------------------------------------
+
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+result = num1 + num2
+
+print(result)
+
+#-------------------------------------------
+
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+result = int(num1) + int(num2)
+
+print(result)
+
+#-------------------------------------------
+
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+result = float(num1) + float(num2)
+
+print(result)
+
+#-------------------------------------------
+
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+result = float(num1) + float(num2)
+
+print("resultado: " + str(result))
